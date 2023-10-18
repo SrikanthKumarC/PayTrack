643d36bd10c66ea86f108ac6d480ac089fa94cdf
Srikanth Cheruku <wpsrikanth@gmail.com>
2023-10-18 15:44:18 +0530

    fix 404: build fails with single quote fixed.

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ function NotFound() {
       <div className="text-center text-yellow-500 mt-20">
         <h1 className="text-8xl dark:text-gray-100 text-black mb-5">404</h1>
         <i className="ri-error-warning-fill text-center text-5xl"></i>
-        <p className=" text-xl mt-4 text-gray-600 dark:text-gray-400 sm:px-0 px-4">The page you are looking for doesn't exist!</p>
+        <p className=" text-xl mt-4 text-gray-600 dark:text-gray-400 sm:px-0 px-4">The page you are looking for does not exist!</p>
       </div>
     </div>
   );
