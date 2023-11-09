e8e7937c9e54e9e8f2ff2ea6a0927830fb8dfc69
Srikanth Cheruku <wpsrikanth@gmail.com>
2023-11-10 00:13:41 +0530

    loading state to signup and fixes

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Link href={'/about'} title="about">
+      <Link href={"/about"} title="about">
         <i class="ri-information-line absolute cursor-pointer text-3xl sm:text-4xl text-gray-300 font-thin right-0 mr-4"></i>
       </Link>
       <body className="dark:bg-neutral-950 relative dark:text-white p-4 pb-20 bg-stone-50">
